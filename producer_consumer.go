8605e03c8091a73d47e9057a2b4571587c675581
zhiruchen <zhiruchen1992@foxmail.com>
2017-10-21 21:27:46 +0800

    modify producer comsumer

--- producer_consumer.go
+++ producer_consumer.go
@@ -12,20 +12,18 @@ func produce(ch chan int, done chan bool) {
 	done <- true
 }
 
-func consume(ch chan int, done chan bool) {
-	for i := 0; i < 100; i += 10 {
+func consume(ch chan int) {
+	for {
 		fmt.Printf("consume %d\n", <-ch)
 	}
-	done <- true
 }
 
 func main() {
-	done := make(chan bool, 2)
+	done := make(chan bool)
 	ch := make(chan int)
 
 	go produce(ch, done)
-	go consume(ch, done)
+	go consume(ch)
 
-	<-done
 	<-done
 }
