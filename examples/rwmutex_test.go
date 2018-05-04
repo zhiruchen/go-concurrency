20f9f899b3e44672be40e4fde5880ff399cebc41
zhiruchen <zhiruchen1992@foxmail.com>
2018-05-04 10:24:08 +0800

    Add rwmuext test

--- examples/rwmutex_test.go
+++ examples/rwmutex_test.go
@@ -5,9 +5,100 @@ import (
 )
 
 func TestConcurrentMap_Get(t *testing.T) {
+	cases := []struct {
+		m   map[string]int
+		key string
+		val int
+		ok  bool
+	}{
+		{
+			m: map[string]int{
+				"a": 1,
+				"b": 2,
+				"c": 9,
+			},
+			key: "b",
+			val: 2,
+			ok:  true,
+		},
+		{
+			m: map[string]int{
+				"a": 1,
+				"b": 2,
+				"c": 9,
+			},
+			key: "c",
+			val: 9,
+			ok:  true,
+		},
+		{
+			m: map[string]int{
+				"a": 1,
+				"b": 2,
+				"c": 9,
+			},
+			key: "x",
+			val: 0,
+			ok:  false,
+		},
+	}
+
+	for _, cc := range cases {
+		cmap := newCMap()
+
+		for k, v := range cc.m {
+			cmap.Put(k, v)
+		}
+
+		v, ok := cmap.Get(cc.key)
+		if v != cc.val || ok != cc.ok {
+			if v != cc.val {
+				t.Errorf("expect: %v, get: %v\n", cc.val, v)
+			}
+
+			if ok != cc.ok {
+				t.Errorf("expect: %v, get: %v\n", cc.ok, ok)
+			}
+		}
+	}
 }
 
 func TestConcurrentMap_Put(t *testing.T) {
+	cases := []struct {
+		m     map[string]int
+		count int
+	}{
+		{
+			m: map[string]int{
+				"a": 1,
+				"b": 2,
+				"c": 9,
+			},
+			count: 3,
+		},
+		{
+			m: map[string]int{
+				"a": 1,
+			},
+			count: 1,
+		},
+		{
+			m:     map[string]int{},
+			count: 0,
+		},
+	}
+
+	for _, cc := range cases {
+		cmap := newCMap()
+		for k, v := range cc.m {
+			cmap.Put(k, v)
+		}
+
+		count := cmap.Count()
+		if count != cc.count {
+			t.Errorf("expect: %v, get: %v\n", cc.count, count)
+		}
+	}
 }
 
 func TestConcurrentMap_Remove(t *testing.T) {
